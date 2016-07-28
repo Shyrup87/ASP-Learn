5efb98a7f827bdbba698166e1851dc96463df1bb
shyrup <shyrup87@gmail.com>
2016-07-28 11:08:16 +0300

    first deploy

---
+++ PartyInvites/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PartyInvites.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="PartyStyles.css" />
+</head>
+<body>
+    <form id="rsvpform" runat="server">
+        <div>
+            <h1>Hello</h1>
+            <p>This is a new web form</p>
+        </div>
+        <asp:ValidationSummary ID="validationSummary" runat="server" ShowValidationErrors="true" />
+        <div><label>Your name:</label><input type="text" id="name" runat="server"/></div>
+        <div><label>Your email:</label><input type="text" id="email" runat="server"/></div>
+        <div><label>Your phone:</label><input type="text" id="phone" runat="server" /></div>
+        <div>
+            <label>Will you attend?</label>
+            <select id="willattend" runat="server">
+                <option value="">Choose an Option</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select>
+        </div>
+        <div>
+            <button type="submit">Submit RSVP</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PartyInvites/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PartyInvites")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Beloil")]
+[assembly: AssemblyProduct("PartyInvites")]
+[assembly: AssemblyCopyright("Copyright © Beloil 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a5d8cab-d223-42d1-bfa9-91fad682f81b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
